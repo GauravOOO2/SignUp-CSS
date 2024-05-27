9a7b329baa3bc9ef4b28cc05f3d6cb1e4760ed82
Gaurav0002 <gauravking918@gmail.com>
2024-05-27 13:14:01 +0530

    Styles update

---
+++ script.js
@@ -0,0 +1,5 @@
+const container = document.querySelector('.container')
+const signUp_button =  document.querySelector('.green-bg button')
+signUp_button.addEventListener('click', ()=>{
+    container.classList.toggle('change')
+})
