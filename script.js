0f2917e1d869b1141e622ed4561e60e0ec069c15
Gaurav0002 <gauravking918@gmail.com>
2024-05-27 15:10:20 +0530

    Completed Successfully

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const container = document.querySelector('.container')
-const signUp_button =  document.querySelector('.green-bg button')
-signUp_button.addEventListener('click', ()=>{
+const signup_button = document.querySelector('.green_bg button')
+
+signup_button.addEventListener('click', ()=>{
     container.classList.toggle('change')
 })
